e5c4bf4e4930516a683a351f7b17f7a2ce747718
arkhipov.iv99@mail.ru <arkhipov.iv99@mail.ru>
2024-08-28 15:26:11 +0300

    Enable tests

---
+++ tests/local_voting_protocol.py
@@ -0,0 +1,29 @@
+import numpy as np
+from scripts.algorithms.local_voting_protocol import LocalVotingProtocol
+from scripts.models.parameters import Parameters
+
+def test_local_voting_protocol():
+    generate = True
+    num_agents = 5
+    productivities = [10] * num_agents
+    num_steps = 20
+
+    pars = Parameters()
+    pars.n = num_agents
+    Adj = np.array([
+        [0, 0, 1, 1, 0],
+        [0, 0, 0, 1, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 0, 0, 0],
+        [0, 1, 1, 0, 0]
+    ])
+    pars.b = Adj / 2
+
+    pars.neib_add = 5
+    pars.add_neib_val = 0.3
+    pars.params_dict = {
+        "h": 0.2,
+    }
+
+    alg_lvp = LocalVotingProtocol(params=pars)
+    alg_lvp.run(num_steps=num_steps, generate=generate, productivities=productivities)
