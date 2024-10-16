af29ae331c659c7816e68c023ece51368b82ae22
arkhipov.iv99@mail.ru <arkhipov.iv99@mail.ru>
2024-10-16 13:19:49 +0300

    Fix linter

--- scripts/algorithms/abstract.py
+++ scripts/algorithms/abstract.py
@@ -107,8 +107,8 @@ class LbAlgorithm:
     def create_agents(self, num_steps, generate, productivities):
         task_pool = self.create_task_pool(num_steps, generate)
         self.agents = [
-            Agent(id, productivities[id], num_steps=num_steps, generate=generate
-                , task_pool=task_pool, random_task_generate=self.random_task_generate)
+            Agent(id, productivities[id], num_steps=num_steps, generate=generate, 
+                  task_pool=task_pool, random_task_generate=self.random_task_generate)
             for id in range(self.n)
         ]
         self.theta_hat = np.matrix([len(agent.tasks) for agent in self.agents]).transpose()
